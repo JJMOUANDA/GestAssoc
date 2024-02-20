1f6247303c23dc558d6552bfe2fcc3bb2a1bc9ed
mathurin <mathurinhauville@gmail.com>
2024-02-20 20:46:20 +0100

    chore: backup de test bdd mariadb

---
+++ BDD/mariadb/backup/backup_mathurin_2024-02-20_20-42-51.sql
@@ -0,0 +1,138 @@
+-- MariaDB dump 10.19  Distrib 10.5.23-MariaDB, for debian-linux-gnu (aarch64)
+--
+-- Host: localhost    Database: gestion_association
+-- ------------------------------------------------------
+-- Server version	10.5.23-MariaDB-1:10.5.23+maria~ubu2004
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `evenement`
+--
+
+DROP TABLE IF EXISTS `evenement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `evenement` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `dateHeureDebut` datetime NOT NULL,
+  `dateHeureFin` datetime NOT NULL,
+  `maxParticipants` int(11) NOT NULL,
+  `lieuId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lieuId` (`lieuId`),
+  CONSTRAINT `evenement_ibfk_1` FOREIGN KEY (`lieuId`) REFERENCES `lieu` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `evenement`
+--
+
+LOCK TABLES `evenement` WRITE;
+/*!40000 ALTER TABLE `evenement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `evenement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inscription`
+--
+
+DROP TABLE IF EXISTS `inscription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inscription` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `membreId` int(11) NOT NULL,
+  `evenementId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_inscription` (`membreId`,`evenementId`),
+  KEY `evenementId` (`evenementId`),
+  CONSTRAINT `inscription_ibfk_1` FOREIGN KEY (`membreId`) REFERENCES `membre` (`id`),
+  CONSTRAINT `inscription_ibfk_2` FOREIGN KEY (`evenementId`) REFERENCES `evenement` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inscription`
+--
+
+LOCK TABLES `inscription` WRITE;
+/*!40000 ALTER TABLE `inscription` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inscription` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lieu`
+--
+
+DROP TABLE IF EXISTS `lieu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lieu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `capaciteAccueil` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lieu`
+--
+
+LOCK TABLES `lieu` WRITE;
+/*!40000 ALTER TABLE `lieu` DISABLE KEYS */;
+INSERT INTO `lieu` VALUES (1,'hdhh','hdhdh',2);
+/*!40000 ALTER TABLE `lieu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `membre`
+--
+
+DROP TABLE IF EXISTS `membre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `membre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `dateNaissance` date NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `motDePasse` char(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `membre`
+--
+
+LOCK TABLES `membre` WRITE;
+/*!40000 ALTER TABLE `membre` DISABLE KEYS */;
+/*!40000 ALTER TABLE `membre` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-20 19:42:51
