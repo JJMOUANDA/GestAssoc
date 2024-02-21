9a26ad2d3dde90fda8aaded84b5694315c425e06
mathurin <mathurinhauville@gmail.com>
2024-02-21 21:45:40 +0100

    feature: pojo

---
+++ message-api/src/main/java/org/example/messageapi/Inscription.java
@@ -0,0 +1,47 @@
+package org.example.messageapi;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "inscription")
+public class Inscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "evenementId")
+    private Evenement evenement;
+
+    @ManyToOne
+    @JoinColumn(name = "MemberId")
+    private Membre membre;
+
+    public Inscription() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Evenement getEvenement() {
+        return evenement;
+    }
+
+    public void setEvenement(Evenement evenement) {
+        this.evenement = evenement;
+    }
+
+    public Membre getMembre() {
+        return membre;
+    }
+
+    public void setMembre(Membre membre) {
+        this.membre = membre;
+    }
+}
