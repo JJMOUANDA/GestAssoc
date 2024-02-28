ad1ac341b2fdbdfe622e39f31caf4c8e95568609
mathurin <mathurinhauville@gmail.com>
2024-02-28 12:58:03 +0100

    feature: création de commentaire depuis un formulaire

---
+++ Front-api/Front/src/components/FormulaireCommentaire.vue
@@ -0,0 +1,57 @@
+<script setup>
+import CommentaireService from '../services/CommentaireService.js';
+
+let idEvenement = '';
+let idAuteur = '';
+let texteCommentaire = '';
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  try {
+    await CommentaireService.createCommentaire(idEvenement, idAuteur, texteCommentaire);
+    idEvenement = '';
+    idAuteur = '';
+    texteCommentaire = '';
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
+
+<template>
+  <form @submit="handleSubmit">
+    <fieldset>
+      <legend>Ajouter un commentaire</legend>
+      <div class="form-group">
+        <label for="exampleSelect1" class="form-label mt-4">Evénement</label>
+        <select v-model="idEvenement" class="form-select" id="selectEvenement">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="exampleSelect1" class="form-label mt-4">Auteur</label>
+        <select v-model="idAuteur" class="form-select" id="selectAuteur">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="exampleTextarea" class="form-label mt-4">Commentaire</label>
+        <textarea v-model="texteCommentaire" class="form-control" id="commentaire" rows="3"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Envoyer</button>
+    </fieldset>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
--- Front-api/Front/src/pages/Commentaire.vue
+++ Front-api/Front/src/pages/Commentaire.vue
@@ -1,10 +1,12 @@
 <script setup>
 
 import CommentaireList from "@/components/CommentaireList.vue";
+import FormulaireCommentaire from "@/components/FormulaireCommentaire.vue";
 </script>
 
 <template>
   <CommentaireList/>
+  <FormulaireCommentaire/>
 </template>
 
 <style scoped>
