6d7ad2b478b8ae9db0163f18feaaa96d9ae00cdf
MoetazJ <jaouedmoetaz@gmail.com>
2024-02-28 20:41:16 +0100

    Ajout de maps pour chaque lieu

--- Front-api/Front/src/pages/Evenement.vue
+++ Front-api/Front/src/pages/Evenement.vue
@@ -1,10 +1,12 @@
 <script setup>
 
 import Evenements from "@/components/Evenements.vue";
+import LieuList from "@/components/LieuList.vue";
 </script>
 
 <template>
   <Evenements/>
+  <LieuList/>
 </template>
 
 <style scoped>
