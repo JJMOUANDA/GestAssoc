65b2169f93ad1ee625ae73c0d583d0197063cc90
mathurin <mathurinhauville@gmail.com>
2024-02-29 14:35:30 +0100

    feature: modifier et supprimer commentaire avec message succès

--- Front-api/Front/src/components/CommentaireList.vue
+++ Front-api/Front/src/components/CommentaireList.vue
@@ -2,7 +2,6 @@
 
 import {ref, onMounted} from 'vue';
 import CommentaireService from '../services/CommentaireService.js';
-import {formatDate} from "@/util/dateUtil.js";
 import CommentaireCard from "@/components/CommentaireCard.vue";
 
 const commentaires = ref([]);
@@ -16,13 +15,24 @@ onMounted(async () => {
   }
 });
 
+import {useRoute} from 'vue-router';
+import MessageSucces from "@/components/MessageSucces.vue";
+
+let message = "";
+const route = useRoute();
+
+if (route.query.message) {
+  message = route.query.message;
+}
+console.log(message);
+
 </script>
 
 <template>
+  <MessageSucces v-if="message" :message="message" />
   <div>
     <h2>Liste des Commentaires</h2>
     <ul>
-
       <template v-for="commentaire in commentaires" :key="commentaire.id">
         <CommentaireCard :id="commentaire.id" :auteur="commentaire.auteurId" :evenement="commentaire.evenementId"
                          :texte="commentaire.texte" :date="commentaire.date"/>
