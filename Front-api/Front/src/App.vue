5dca1494aca15817eb82e7012d9e4b8378e5ded7
mathurin <mathurinhauville@gmail.com>
2024-03-04 21:43:46 +0100

    feature: component navbar

--- Front-api/Front/src/App.vue
+++ Front-api/Front/src/App.vue
@@ -1,24 +1,9 @@
 <script setup>
+
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-    <div class="container-fluid">
-
-      <div class="collapse navbar-collapse" id="navbarColor01">
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">Evénements</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/commentaire">Commentaires</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/membre">Membres</router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <router-view />
+  <Navbar/>
+  <router-view/>
 </template>
